63ed577372c5d7a99e12324b664b644baffec029
Junjun Zhang <junjun.ca@gmail.com>
2021-11-26 21:51:18 -0500

    rename and cleanup

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,7 @@ import PluginManager from '@jbrowse/core/PluginManager'
 import ViewType from '@jbrowse/core/pluggableElementTypes/ViewType'
 import { AbstractSessionModel, isAbstractMenuManager } from '@jbrowse/core/util'
 import { version } from '../package.json'
-import {
-  ReactComponent as HelloViewReactComponent,
-  stateModel as helloViewStateModel,
-} from './HelloView'
+
 import { IsoformInspector } from './IsoformInspector/IsoformInspector'
 import { default as IsoformInspectorModel } from './IsoformInspector/model'
 
